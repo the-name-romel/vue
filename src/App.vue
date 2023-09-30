5c525d447f426c95d7181c89b493dc12820f0ac9
sheenalovendino <slovendino@sorsu.edu.ph>
2023-09-30 21:10:22 +0800

    edited

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,12 @@
-<template></template>
+<script setup>
+import {ref} from 'vue' 
+
+const input = ref('Hi')
+</script>
+<template>
+    <select v-model="input">
+        <option value="web">web</option>
+        <option value="design">design</option>
+    </select>
+    <p>p: {{ input }}</p>
+</template>
